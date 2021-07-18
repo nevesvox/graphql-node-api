672d921421f2e94d176f9256401418c1583b1d50
Gabriel Neves Maciel <nevesvox@gmail.com>
2021-07-18 15:18:49 -0300

    Ajustes

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import { normalizePort, onError, onListening } from './utils/utils';
 const server = http.createServer(app);
 const port = normalizePort(process.env.port || 3000);
 
-server.listen(port);
-server.on('error', onError(server));
+server.listen(port);  
+server.on('error', onError(server));  
 server.on('listening', onListening(server))
